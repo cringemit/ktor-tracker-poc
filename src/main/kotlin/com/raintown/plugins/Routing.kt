34753d122dd8170357516d712edc689b5b28454c
rainyrobs <raintown-code@proton.me>
2023-02-04 23:34:06 +0000

    Added routes for showing and creating new foods

--- src/main/kotlin/com/raintown/plugins/Routing.kt
+++ src/main/kotlin/com/raintown/plugins/Routing.kt
@@ -1,11 +1,16 @@
 package com.raintown.plugins
 
+import com.raintown.models.Food
 import com.raintown.models.foodStorage
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.resources.Resources
 import io.ktor.server.application.*
 import io.ktor.server.freemarker.FreeMarkerContent
+import io.ktor.server.request.receiveParameters
+import io.ktor.server.util.getOrFail
+import java.util.UUID
 
 fun Application.configureRouting() {
     install(Resources)
@@ -20,6 +25,29 @@ fun Application.configureRouting() {
             get {
                 call.respond(FreeMarkerContent("food/index.ftl", mapOf("foods" to foodStorage)))
             }
+
+            get("{id}") {
+                val food = foodStorage.find { it.id == UUID.fromString(call.parameters["id"]) }
+                    ?: call.respond(HttpStatusCode.NotFound, "Failed to find a food with that id")
+                call.respond(FreeMarkerContent("food/show.ftl", mapOf("food" to food)))
+            }
+
+            route("new") {
+                get {
+                    call.respond(FreeMarkerContent("food/new.ftl", null))
+                }
+
+                post {
+                    val parameters = call.receiveParameters()
+                    val food = Food.named(
+                        itemName = parameters.getOrFail("item-name"),
+                        brandName = parameters.getOrFail("brand-name")
+                    )
+                    foodStorage.add(food)
+                    call.respondRedirect("${food.id}")
+                }
+            }
+
         }
     }
 }
