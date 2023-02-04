d90f046603e9521e479fef3985e5ef891e114c48
rainyrobs <raintown-code@proton.me>
2023-02-04 22:13:13 +0000

    Created a route for food resource and redirect to it from root

--- src/main/kotlin/com/raintown/plugins/Routing.kt
+++ src/main/kotlin/com/raintown/plugins/Routing.kt
@@ -2,12 +2,23 @@ package com.raintown.plugins
 
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
-import io.ktor.server.resources.*
-import io.ktor.resources.*
 import io.ktor.server.resources.Resources
-import kotlinx.serialization.Serializable
 import io.ktor.server.application.*
+import io.ktor.server.freemarker.FreeMarkerContent
 
 fun Application.configureRouting() {
     install(Resources)
+    routing {
+        route("/") {
+            get {
+                call.respondRedirect("food")
+            }
+        }
+
+        route("food") {
+            get {
+                call.respond(FreeMarkerContent("food/index.ftl", null))
+            }
+        }
+    }
 }
---
+++ src/main/resources/templates/food/index.ftl
@@ -0,0 +1,26 @@
+<#import "../_base.ftl" as base />
+<@base.header>
+    <h1>Food Database</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Brand Name</th>
+                <th>Item Name</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Generic</td>
+                <td>Apple</td>
+            </tr>
+            <tr>
+                <td>Kellog's</td>
+                <td>Cornflakes</td>
+            </tr>
+            <tr>
+                <td>Generic</td>
+                <td>Parsnip</td>
+            </tr>
+        </tbody>
+    </table>
+</@base.header>
