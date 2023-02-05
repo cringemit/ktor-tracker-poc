015f2231c1121ca6774e6140b99b0488600c28b5
rainyrobs <raintown-code@proton.me>
2023-02-05 09:40:35 +0000

    Added junit 5 and jsoup, removed old junit

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,5 +28,7 @@ dependencies {
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+    testImplementation("org.jsoup:jsoup:1.15.3")
 }
